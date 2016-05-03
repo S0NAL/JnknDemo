90baf5d756319196bbf48752b7e956f7f173473e
S0NAL <paghadalsonal@gmail.com>
2016-05-03 10:46:01 +0530

    * HomePage.cs: * JenkinsDemo.cs: * JenkinsDemo.csproj: * Kixie.jpg: * Kixie.jpg: * Kixie@2x.jpg: * Contents.json: * Contents.json: * Contents.json:
    * JenkinsDemo.iOS.csproj: Added Home screen

---
+++ JenkinsDemo/HomePage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace JenkinsDemo
+{
+	public class HomePage : ContentPage
+	{
+		public HomePage ()
+		{
+
+
+//			var menuImage = new Image { WidthRequest = 20d, HeightRequest = 20d };
+//			menuImage.Source = ImageSource.FromFile("ic_drawer_toggle.png");
+
+
+			Image kixie = new Image {
+				WidthRequest=200d, HeightRequest=200d,
+				BackgroundColor = Color.Aqua
+			};
+			kixie.Source = ImageSource.FromFile ("kixie.jpg");
+
+			Content = new StackLayout {
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center,
+				Children = {
+					kixie
+				}
+			};
+		}
+	}
+}
+
